139b74aaa6f48de4e4a955cee04d0ba3d7eb45ae
David Stephenson <davidstephenson8@gmail.com>
2022-11-23 06:20:20 -0700

    refactoring with calculator

--- Lesson2/calculator.rb
+++ Lesson2/calculator.rb
@@ -1,18 +1,87 @@
-Kernel.puts(">> Give two numbers that we'll use to perform calculations on")
-number1 = Kernel.gets().chomp().to_i()
-number2 = Kernel.gets().chomp().to_i()
-
-Kernel.puts(">> What function would you like to perform on these numbers? Subtraction, multiplication, division or addition?")
-operation = Kernel.gets().chomp().downcase()
-
-if operation == "subtraction"
-	puts number1 - number2
-
-elsif operation == "division"
-	puts number1.to_f / number2.to_f	
-elsif	operation == "addition"
-	puts number1 + number2
-else
-	puts number1 * number2
+def prompt(message)
+	Kernel.puts(">> #{message}")
 end
 
+def valid_number?(num)
+	num.to_i() != 0
+end
+
+prompt("Welcome to calculator! Enter your name:")
+
+name = ''
+loop do
+	name = Kernel.gets().chomp
+
+	if name.empty?()
+		prompt("Make sure to use a valid name.")
+	else
+		break
+	end
+end
+
+loop do
+
+	number1 = ''
+	number2 = ''
+
+	loop do
+		prompt("What's the first number?")
+		number1 = Kernel.gets().chomp().to_i()
+		if valid_number?(number1)
+			break
+		else
+			prompt("Invalid number")
+		end
+	end
+
+	loop do
+		prompt("What's the second number?")
+		number2 = Kernel.gets().chomp().to_i()
+
+		if valid_number?(number2)
+			break	
+		else
+			prompt("Invalid number")
+		end
+	end
+
+	operator_prompt = <<-MSG
+		What operation would you like to perform?
+		addition
+		subtraction
+		multiplication
+		division
+		MSG
+	
+	operation = ''
+	loop do
+		prompt(operator_prompt)
+		operation = Kernel.gets().chomp().downcase()
+		if %w(addition subtraction division multiplication).include?(operation)
+			break
+		else
+			prompt("I don't know how to do that. Addition, subtraction, division or multiplication, please!")
+		end
+	end
+
+	prompt("#{operation} is happening!")
+
+	result = case operation 
+						when "subtraction"
+							result = number1 - number2
+						when "division"
+							result = number1.to_f / number2.to_f	
+						when "addition"
+							result = number1 + number2
+						when "multiplication"
+							result = number1 * number2
+	end
+
+	prompt("The result is #{result}")
+
+	prompt("Do you want to perform another calculation? (Y to calculate again)")
+	answer = Kernel.gets().chomp()
+	break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thanks for calculating, have a calculator-y day!")
