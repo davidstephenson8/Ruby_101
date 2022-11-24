718ba8478f3becbbd38103faea5ed850d160788c
David <davidstephenson@Davids-MacBook-Air.local>
2022-11-24 09:32:56 -0700

    Refactoring with rubocop

--- Lesson2/calculator.rb
+++ Lesson2/calculator.rb
@@ -1,87 +1,86 @@
 def prompt(message)
-	Kernel.puts(">> #{message}")
+  Kernel.puts(">> #{message}")
 end
 
 def valid_number?(num)
-	num.to_i() != 0
+  num.to_i() != 0
 end
 
 prompt("Welcome to calculator! Enter your name:")
 
 name = ''
 loop do
-	name = Kernel.gets().chomp
+  name = Kernel.gets().chomp
 
-	if name.empty?()
-		prompt("Make sure to use a valid name.")
-	else
-		break
-	end
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
 end
 
 loop do
+  number1 = ''
+  number2 = ''
 
-	number1 = ''
-	number2 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp().to_i()
+    if valid_number?(number1)
+      break
+    else
+      prompt("Invalid number")
+    end
+  end
 
-	loop do
-		prompt("What's the first number?")
-		number1 = Kernel.gets().chomp().to_i()
-		if valid_number?(number1)
-			break
-		else
-			prompt("Invalid number")
-		end
-	end
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp().to_i()
 
-	loop do
-		prompt("What's the second number?")
-		number2 = Kernel.gets().chomp().to_i()
+    if valid_number?(number2)
+      break
+    else
+      prompt("Invalid number")
+    end
+  end
 
-		if valid_number?(number2)
-			break	
-		else
-			prompt("Invalid number")
-		end
-	end
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+  addition
+  subtraction
+  multiplication
+  division
+  MSG
 
-	operator_prompt = <<-MSG
-		What operation would you like to perform?
-		addition
-		subtraction
-		multiplication
-		division
-		MSG
-	
-	operation = ''
-	loop do
-		prompt(operator_prompt)
-		operation = Kernel.gets().chomp().downcase()
-		if %w(addition subtraction division multiplication).include?(operation)
-			break
-		else
-			prompt("I don't know how to do that. Addition, subtraction, division or multiplication, please!")
-		end
-	end
+  operation = ''
+  loop do
+    prompt(operator_prompt)
+    operation = Kernel.gets().chomp().downcase()
+    if %w(addition subtraction division multiplication).include?(operation)
+      break
+    else
+      prompt("Addition, subtraction, division or multiplication, please!")
+    end
+  end
 
-	prompt("#{operation} is happening!")
+  prompt("#{operation} is happening!")
 
-	result = case operation 
-						when "subtraction"
-							result = number1 - number2
-						when "division"
-							result = number1.to_f / number2.to_f	
-						when "addition"
-							result = number1 + number2
-						when "multiplication"
-							result = number1 * number2
-	end
+  result = case operation
+           when "subtraction"
+             number1 - number2
+           when "division"
+             number1.to_f / number2.to_f
+           when "addition"
+             number1 + number2
+           when "multiplication"
+             number1 * number2
+           end
 
-	prompt("The result is #{result}")
+  prompt("The result is #{result}")
 
-	prompt("Do you want to perform another calculation? (Y to calculate again)")
-	answer = Kernel.gets().chomp()
-	break unless answer.downcase().start_with?('y')
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thanks for calculating, have a calculator-y day!")
