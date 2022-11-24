bb8085a5d6974beb8630e50bb264b574e3ab8301
David <davidstephenson@Davids-MacBook-Air.local>
2022-11-24 10:06:04 -0700

    Worked on a few of the small problems.

---
+++ Small_Problems/Documentation_Again/1.rb
@@ -0,0 +1,7 @@
+=begin
+file::path is a class method. It returns a string representation of the path.
+file#path is an instance method. It returns a string of the path
+ that's used in the creation of a new file. 
+
+
+=end
