41f590149638977f67b9097f3a58dddc4f4e58b5
David Stephenson <davidstephenson8@gmail.com>
2022-11-28 21:20:07 -0700

    figured out yml, quick and dirty mortgage program.

--- Lesson2/mortgage_calculator.rb
+++ Lesson2/mortgage_calculator.rb
@@ -19,14 +19,39 @@ C
 =end
 
 require 'yaml'
-message = YAML.load_file(mortgage_messages.yml)
-
-puts(message)
+MESSAGES = YAML.load_file('mortgage_messages.yml')
+puts MESSAGES.inspect
 
 def prompt(message)
 	puts "$$$$ #{message}"
 end
 
+def apr_to_monthly_interest_rate(apr)
+	monthly_interest_rate = apr / 12
+end
 
+def years_to_months(years)
+	months = years * 12
+end
 
-monthly_payment = loan_amount * (monthly_interest_rate/ ( 1= (1 + monthly_interest_rate)**(-loan_duration)))
+prompt(MESSAGES['welcome'])
+prompt(MESSAGES['introduction'])
+
+prompt(MESSAGES['loan_amount'])
+loan_amount = gets.chomp.to_f
+
+prompt(MESSAGES['APR'])
+apr = gets.chomp.to_f/100
+
+prompt(MESSAGES['loan_duration'])
+loan_duration_years = gets.chomp.to_f
+
+loan_duration = years_to_months(loan_duration_years)
+
+monthly_interest_rate = apr_to_monthly_interest_rate(apr)
+
+monthly_payment = loan_amount * (monthly_interest_rate/ ( 1 - (1 + monthly_interest_rate)**(-loan_duration)))
+
+puts(MESSAGES['monthly_payment'] + "#{monthly_payment}")
+
+puts "loan amount is #{loan_amount} monthly interest rate is #{monthly_interest_rate} loan duration is #{loan_duration}"
