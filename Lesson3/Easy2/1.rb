93d987debe81d4d1309c3c5e02e1dc8eedb04df2
David <davidstephenson@Davids-MacBook-Air.local>
2022-12-08 12:29:15 -0700

    Finished Easy2

---
+++ Lesson3/Easy2/1.rb
@@ -0,0 +1,6 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+p ages["Spot"]
+p ages.include?("Spot")
+p ages.key?("Spot")
+p ages.member?("Spot")
---
+++ Lesson3/Easy2/7.rb
@@ -0,0 +1,6 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+others = %w(Dino Hoppy)
+
+flintstones.concat(others)
+
+p flintstones
