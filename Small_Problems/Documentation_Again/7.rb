71d914c44c5183b85fb435ff1dee7865b73cf82e
David <davidstephenson@Davids-MacBook-Air.local>
2022-12-08 12:55:51 -0700

    missed a few of therse last time

---
+++ Small_Problems/Documentation_Again/7.rb
@@ -0,0 +1,4 @@
+#did the exercise in irb
+
+# public_methods(false) returns all possible methods that you can call on a specific class of object that
+# are unique to that class or can be overwitten 
---
+++ Small_Problems/Documentation_Again/9.rb
@@ -0,0 +1,4 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# Documentation under Psych method. 
